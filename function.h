feac08803ac13246fee7eb43b59cb837e78b50e7
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-10-17 19:49:54 +0300

    Function name change

--- function.h
+++ function.h
@@ -6,10 +6,10 @@ SPDX-License-Identifier: LGPL-2.1-only
 #define FUNCTION_H
 #include "internals.h"
 variable_data *variable_nodes(s_expression *subexps);
-void replace_unknowns(variable_data *pointers, double value);
+void replace_variable(variable_data *pointers, double value);
 double calculate_function(char *exp, int a, int b, double x);
 bool derivative_processor(char *exp);
-bool derivative(char *exp);
+double derivative(char *exp);
 double integrate(char *exp, double a, double b);
-bool integral_processor(char *exp);
+double integral_processor(char *exp);
 #endif
