adb916852b0d5c70a8a91472ecaa4e362ab428a6
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-10-29 12:07:38 +0300

    Remove outdated variables

--- include/libtmsolve.h
+++ include/libtmsolve.h
@@ -9,12 +9,4 @@ SPDX-License-Identifier: LGPL-2.1-only
 #include "libtmsolve/internals.h"
 #include "libtmsolve/function.h"
 #include "libtmsolve/matrix.h"
-// Defining the clear console command
-/*
-To calculate needed exp size :
-expression_length *(max_length) + (max_length - 1) * (implicit_multiplication_overhead) + 1(null terminator)*/
-// Previous answer global variable
-#define EXP_SIZE(size) ((size)*23 + (size - 1) * 3 + 1)
-extern double complex ans;
-extern int g_var_count;
 #endif
