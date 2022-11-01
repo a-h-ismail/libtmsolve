7ac7debb012b7c254a4f87295ad9e648b1112f60
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-11-01 15:08:56 +0200

    Add factor structure

--- scientific.h
+++ scientific.h
@@ -10,6 +10,11 @@ SPDX-License-Identifier: LGPL-2.1-only
 #include <math.h>
 #include <complex.h>
 #include "internals.h"
+typedef struct int_factor
+{
+    int factor;
+    int power;
+} int_factor;
 extern char *function_name[];
 extern double (*math_function[])(double);
 extern char *ext_function_name[];
@@ -18,7 +23,7 @@ int s_process(char *exp, int p);
 double complex evaluate(math_expr *math_struct);
 math_expr *parse_expr(char *exp, bool enable_variables, bool enable_complex);
 double factorial(double value);
-double calculate_expr(char *exp);
+double complex calculate_expr(char *exp, bool enable_complex);
 double evaluate_region(char *exp, int a, int b);
 void scientific_complex_picker(char *exp);
 void delete_math_expr(math_expr *math_struct);
@@ -26,9 +31,9 @@ int priority_test(char operator1, char operator2);
 void priority_fill(node *list, int op_count);
 int subexp_start_at(s_expression *expression, int start, int current_s_exp, int mode);
 int s_exp_ending(s_expression *expression, int end, int current_s_exp, int s_exp_count);
-int32_t *find_factors(int32_t a);
-void fraction_processor(char *exp, bool wasdecimal);
-bool decimal_to_fraction(char *exp, bool inverse_process);
+int_factor *find_factors(int32_t value);
+void reduce_fraction(fraction *fraction_r);
+fraction decimal_to_fraction(double value, bool inverse_process);
 void factorize(char *exp);
 void nroot_solver(char *exp);
 void fraction_reducer(int32_t *f_v1, int32_t *f_v2, int32_t *v1, int32_t *v2);
