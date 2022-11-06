58025c9bed78dfae4b57c6491f281a488100c97c
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-11-06 19:36:13 +0200

    Add missing function declarations

--- matrix.h
+++ matrix.h
@@ -9,6 +9,8 @@ typedef struct matrix_str
     int rows, columns;
     double **data;
 } matrix_str;
+matrix_str *new_matrix(int rows, int columns);
+void delete_matrix(matrix_str *matrix);
 matrix_str *remove_matrix_row_col(matrix_str *matrix, int row, int col);
 matrix_str *matrix_multiply(matrix_str *A, matrix_str *B);
 double matrix_det(matrix_str *A);
