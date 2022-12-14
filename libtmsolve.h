8ae7ab5176bb887fed88537cce85f2b5701ad94f
Ahmad Ismail <ahmadismail2001@gmail.com>
2022-12-14 13:13:20 +0200

    Move the main header to the parent directory.

---
+++ libtmsolve.h
@@ -0,0 +1,16 @@
+/*
+Copyright (C) 2021-2022 Ahmad Ismail
+SPDX-License-Identifier: LGPL-2.1-only
+*/
+#ifndef TMSOLVE_H
+#define TMSOLVE_H
+/**
+ * @file
+ * @brief The main header of libtmsolve, includes all other headers for ease of use.
+*/
+#include "scientific.h"
+#include "string_tools.h"
+#include "internals.h"
+#include "function.h"
+#include "matrix.h"
+#endif
