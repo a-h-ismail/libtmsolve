7124f65e12e8a321a025bac8b6c9059725a29adf
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2022-12-26 12:38:48 +0200

    Add missing linker flag.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir /usr/include/tmsolve
 cp *.h /usr/include/tmsolve
-gcc -fpic -c -O2 *.c -lm
-gcc -shared -o libtmsolve.so *.o
+gcc -fpic -lm -c -O2 *.c
+gcc -shared -lm -o libtmsolve.so *.o
 rm *.o
 mv ./libtmsolve.so /usr/lib64
 echo Done!
