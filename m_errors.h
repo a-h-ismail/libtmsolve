741da2c1d8b164d165d608cee18d4db1f0221738
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2022-12-28 09:27:45 +0200

    Add error and header guards.

--- m_errors.h
+++ m_errors.h
@@ -7,7 +7,8 @@ SPDX-License-Identifier: LGPL-2.1-only
  * @file
  * @brief Contains all error messages.
  */
-
+#ifndef M_ERRORS_H
+#define M_ERRORS_H
 #define PARENTHESIS_MISSING "Expected ( for function call."
 #define PARENTHESIS_EMPTY "Empty parenthesis pair."
 #define PARENTHESIS_NOT_CLOSED "Open parenthesis has no closing parenthesis."
@@ -20,3 +21,6 @@ SPDX-License-Identifier: LGPL-2.1-only
 #define MATH_ERROR "Math error."
 #define RIGHT_OP_MISSING "Missing right operand."
 #define UNDEFINED_SYMBOL "Variable or function isn't defined."
+#define INTERNAL_ERROR "Internal error, please report this as a bug ad send the expression that caused the issue."
+
+#endif
