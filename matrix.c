ecdfb74e15be7af0d5b01554a2e6d5198465f475
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-02-11 11:54:16 +0200

    Add third agrument to prevent confusion.

--- matrix.c
+++ matrix.c
@@ -141,7 +141,7 @@ matrix_str *comatrix(matrix_str *M)
         return NULL;
     if (M->rows < 2 || M->rows != M->columns)
     {
-        error_handler(INVALID_MATRIX, 1, 1);
+        error_handler(INVALID_MATRIX, 1, 1, -1);
         return NULL;
     }
     comatrix = new_matrix(M->rows, M->columns);
