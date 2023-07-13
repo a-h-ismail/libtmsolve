800a6993ad4f028194aead063013db6f923c0ee2
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-07-13 18:15:47 +0300

    Remove custom install script.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir /usr/include/tmsolve
-cp *.h /usr/include/tmsolve
-gcc -fpic -lm -c -O2 -D LOCAL_BUILD *.c
-gcc -shared -lm -o libtmsolve.so *.o
-rm *.o
-mv ./libtmsolve.so /usr/lib64
-echo Done!
