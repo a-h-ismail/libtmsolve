0239aa023449fd072f6c70f4377d26c9466ede71
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-09-19 15:32:40 +0300

    Add new header of math_strs.

--- include/libtmsolve.h
+++ include/libtmsolve.h
@@ -10,26 +10,29 @@ SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #ifndef LOCAL_BUILD
-#include <tmsolve/scientific.h>
-#include <tmsolve/tms_complex.h>
-#include <tmsolve/string_tools.h>
-#include <tmsolve/parser.h>
 #include <tmsolve/evaluator.h>
-#include <tmsolve/internals.h>
 #include <tmsolve/function.h>
-#include <tmsolve/matrix.h>
+#include <tmsolve/internals.h>
 #include <tmsolve/m_errors.h>
+#include <tmsolve/matrix.h>
+#include <tmsolve/parser.h>
+#include <tmsolve/scientific.h>
+#include <tmsolve/string_tools.h>
+#include <tmsolve/tms_complex.h>
+#include <tmsolve/tms_math_strs.h>
 #include <tmsolve/version.h>
 #else
-#include "scientific.h"
-#include "tms_complex.h"
-#include "string_tools.h"
-#include "parser.h"
 #include "evaluator.h"
-#include "internals.h"
 #include "function.h"
-#include "matrix.h"
+#include "internals.h"
 #include "m_errors.h"
+#include "matrix.h"
+#include "parser.h"
+#include "scientific.h"
+#include "string_tools.h"
+#include "tms_complex.h"
+#include "tms_math_strs.h"
 #include "version.h"
 #endif
+
 #endif
