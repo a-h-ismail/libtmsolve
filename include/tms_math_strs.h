af88d30dacc335b5b1d154208c60c3edaa9b7cb5
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-10-24 17:21:28 +0300

    Add structures for integer only expressions.

--- include/tms_math_strs.h
+++ include/tms_math_strs.h
@@ -30,6 +30,13 @@ typedef struct tms_var
     bool is_constant;
 } tms_var;
 
+/// @brief Runtime variable metadata, 64 bit int variant.
+typedef struct tms_int_var
+{
+    char *name;
+    int64_t value;
+} tms_int_var;
+
 /// @brief Operator node, stores the required metadata for an operator and its operands.
 typedef struct tms_op_node
 {
@@ -82,6 +89,14 @@ typedef union tms_mfunc_ptrs
     tms_ufunc *runtime;
 } fptr;
 
+/// @brief Union to store int function pointers
+typedef union tms_int_functions
+{
+    int64_t (*simple)(int64_t);
+    int64_t (*extended)(tms_arg_list *);
+
+} int_fptr;
+
 #define TMS_NOFUNC 0
 #define TMS_F_REAL 1
 #define TMS_F_CMPLX 2
@@ -120,7 +135,7 @@ typedef struct tms_math_subexpr
     /// Stores the pointer of the function to execute
     fptr func;
 
-    /// Stores the type of the function to execute (0: none, 1: real, 2: cmplx, 3: extended)
+    /// Stores the type of the function to execute
     uint8_t func_type;
 
     /// Enables execution of extended function, used to optimize nested extended functions like integration.
@@ -154,5 +169,79 @@ typedef struct tms_math_expr
     /// Toggles complex support.
     bool enable_complex;
 } tms_math_expr;
+
+/// @brief Operator node, stores the required metadata for an operator and its operands.
+typedef struct tms_int_op_node
+{
+    /// The operator of this op_node.
+    char operator;
+    /// Index of the operator in the expression.
+    int operator_index;
+    /// Index of the op_node in the op_node array.
+    int node_index;
+
+    /// Node operator priority.
+    uint8_t priority;
+
+    int64_t left_operand, right_operand, *result;
+    /// Points to the next op_node in evaluation order.
+    struct tms_int_op_node *next;
+} tms_int_op_node;
+
+typedef struct tms_int_subexpr
+{
+    /// @brief Number of operators in this subexpression.
+    int op_count;
+
+    /// @brief Depth of the subexpression, starts from 0 for the whole expression and adds 1 for each nested parenthesis pair.
+    int depth;
+
+    /// @brief The start index of the actual subexpression to parse,
+    /// skips the function name (if any) and points to the beginning of the numerical expression.
+    int solve_start;
+
+    /// @brief The start index of the subexpression in the expression.
+    /// @details If the subexpression has a function call, the index will be at the first character of the function name, otherwise equals to solve_start.
+    int subexpr_start;
+
+    /// @brief The end index of the subexpression, just before the close parenthesis.
+    int solve_end;
+
+    /// Index of the op_node at which the subexpression parsing starts.
+    int start_node;
+
+    /// The array of int_op_nodes composing this subexpression.
+    struct tms_op_node *nodes;
+
+    /// @brief Set to one of the op_nodes result pointer, indicating that the answer of that node is the answer of this subexpression.
+    int64_t **result;
+
+    /// Stores the pointer of the function to execute
+    int_fptr func;
+
+    /// Stores the type of the function to execute
+    uint8_t func_type;
+
+    /// Enables execution of extended function, used to optimize nested extended functions like integration.
+    bool exec_extf;
+} tms_int_subexpr;
+
+typedef struct tms_int_expr
+{
+    /// The string form of the expression
+    char *str;
+
+    /// The subexpression array created by parsing theexpression.
+    tms_int_subexpr *ptr;
+
+    /// Number of subexpression in this expression
+    int subexpr_count;
+
+    /// Answer of the expression
+    int64_t answer;
+
+    /// Indicates the index of tms_g_int_vars to copy the answer to.
+    int runvar_i;
+} tms_int_expr;
 
 #endif
