af5be06a5493ab06c7ad67a25d929fcf3131484c
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-07-25 18:50:20 +0300

    Remove m_errors.h and add error_handler.h

--- include/libtmsolve.h
+++ include/libtmsolve.h
@@ -10,32 +10,32 @@ SPDX-License-Identifier: LGPL-2.1-only
  */
 
 #ifndef LOCAL_BUILD
+#include <tmsolve/bitwise.h>
+#include <tmsolve/error_handler.h>
 #include <tmsolve/evaluator.h>
 #include <tmsolve/function.h>
+#include <tmsolve/int_parser.h>
 #include <tmsolve/internals.h>
-#include <tmsolve/m_errors.h>
 #include <tmsolve/matrix.h>
 #include <tmsolve/parser.h>
 #include <tmsolve/scientific.h>
 #include <tmsolve/string_tools.h>
 #include <tmsolve/tms_complex.h>
 #include <tmsolve/tms_math_strs.h>
-#include <tmsolve/int_parser.h>
-#include <tmsolve/bitwise.h>
 #include <tmsolve/version.h>
 #else
+#include "bitwise.h"
+#include "error_handler.h"
 #include "evaluator.h"
 #include "function.h"
+#include "int_parser.h"
 #include "internals.h"
-#include "m_errors.h"
 #include "matrix.h"
 #include "parser.h"
 #include "scientific.h"
 #include "string_tools.h"
 #include "tms_complex.h"
 #include "tms_math_strs.h"
-#include "int_parser.h"
-#include "bitwise.h"
 #include "version.h"
 #endif
 
