446a18266240f4438248c5576801abbe8718823a
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-09-12 17:07:56 +0300

    Missing dot.

--- include/m_errors.h
+++ include/m_errors.h
@@ -54,7 +54,7 @@ SPDX-License-Identifier: LGPL-2.1-only
 #define SHIFT_AMOUNT_NEGATIVE "Shift amount can't be negative"
 #define ROTATION_AMOUNT_NEGATIVE "Rotation amount can't be negative"
 #define UNKNOWN_FUNC_ERROR "Function reported an error"
-#define ERROR_DB_NOT_EMPTY "libtmsolve warning: expected an empty error database for this facility Clearing it now!\n"
+#define ERROR_DB_NOT_EMPTY "libtmsolve warning: expected an empty error database for this facility. Clearing it now!\n"
 #define BIT_OUT_OF_RANGE "The bit to mask is out of range"
 #define EXPR_NOT_DETERMINISTIC "The expression should not contain random functions"
 #define INVALID_RANGE "Minimum should be smaller than maximum"
