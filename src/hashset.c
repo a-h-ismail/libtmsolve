9a50c05ed9f5bad197931ced9c017ad3d094150f
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2024-09-16 09:34:23 +0300

    Fix improper use of hashmap leading to segmentation faults.

--- src/hashset.c
+++ src/hashset.c
@@ -8,34 +8,34 @@ SPDX-License-Identifier: LGPL-2.1-only
 
 int _str_compare(const void *a, const void *b, void *udata)
 {
-    const char *va = a, *vb = b;
-    return strcmp(va, vb);
+    char **va = a, **vb = b;
+    return strcmp(*va, *vb);
 }
 
 uint64_t _str_hash(const void *item, uint64_t seed0, uint64_t seed1)
 {
-    const char *v = item;
-    return hashmap_xxhash3(v, strlen(v), seed0, seed1);
+    char **v = item;
+    return hashmap_xxhash3(*v, strlen(*v), seed0, seed1);
 }
 
 hashset *hashset_new()
 {
-    return hashmap_new(sizeof(char *), 0, rand(), rand(), _str_hash, _str_compare, NULL, NULL);
+    return hashmap_new(sizeof(char **), 0, rand(), rand(), _str_hash, _str_compare, NULL, NULL);
 }
 
 const char *hashset_set(hashset *set, char *item)
 {
-    return hashmap_set(set, item);
+    return hashmap_set(set, &item);
 }
 
 const char *hashset_remove(hashset *set, char *item)
 {
-    return hashmap_delete(set, item);
+    return hashmap_delete(set, &item);
 }
 
 const char *hashset_get(hashset *set, char *item)
 {
-    return hashmap_get(set, item);
+    return hashmap_get(set, &item);
 }
 
 char **hashset_to_array(hashset *set, size_t *len, bool sort)
