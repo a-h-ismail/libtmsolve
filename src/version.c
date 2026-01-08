98a697cfe167a19c2a2af522b37359bc1d72ba38
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2026-01-08 20:05:29 +0200

    Set to version 3.1.0

--- src/version.c
+++ src/version.c
@@ -1,6 +1,6 @@
 /*
-Copyright (C) 2023-2025 Ahmad Ismail
+Copyright (C) 2023-2026 Ahmad Ismail
 SPDX-License-Identifier: LGPL-2.1-only
 */
 
-char *tms_lib_version = {"3.0.2"};
+char *tms_lib_version = {"3.1.0"};
